3a403c3b81287a04e581041a778c19560fd0db24
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-20 19:23:33 -0300

    Update 1-array.js

--- Introducao_JavaScript/JavaScript_ES6_essicial/script/Aula04-Parte1/1-array.js
+++ Introducao_JavaScript/JavaScript_ES6_essicial/script/Aula04-Parte1/1-array.js
@@ -18,3 +18,40 @@ const almoco = ['feijão', 'arroz', 'carne'];
 
 const refeicoes1 = cafaDaManha.concat(almoco);
 const refeicoes2 = almoco.concat(cafaDaManha);
+
+arr1.push(4, 5);
+
+// forEach
+arr1.forEach((value, index) => {
+    console.log(`${index + 1}º = ${value}`);
+});
+
+// map
+console.log(arr1.map(value => value * 2));
+
+console.log(arr1);
+
+// flat
+const novo_array = [56, 78, [34, 57], [90, 98, 100]];
+console.log(novo_array);
+console.log(novo_array.flat());
+
+// flatMap
+console.log(novo_array.flatMap(value => [value * 2]));
+
+// keys
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
