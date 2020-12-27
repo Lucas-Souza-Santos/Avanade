ceb5d6864a157486049c85d8514a22993e048b7c
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-27 18:49:53 -0300

    novos

--- Introducao_Ao_dotnet/Codigo/Program.cs
+++ Introducao_Ao_dotnet/Codigo/Program.cs
@@ -6,7 +6,13 @@ namespace Codigo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
         }
     }
 }
