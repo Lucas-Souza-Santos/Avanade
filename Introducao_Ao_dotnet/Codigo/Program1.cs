25f6e84bb0990df6086fe75127456c316ecab39e
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-27 23:09:29 -0300

    final da aula 04

---
+++ Introducao_Ao_dotnet/Codigo/Program1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program1{
+    static void Main(String[] args){
+        double x = 0.0;
+        double y = 8.0;
+        double Resultado = x / y;
+        
+
+        Console.WriteLine("Resultado: " + Resultado);
+    }
+} 
