305e8a3ad0027639dcf7c99ea5df3cedff0b694f
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-28 07:44:30 -0300

    final da aula 07

--- Introducao_Ao_dotnet/Anotacoes/pasta7/Revisao/Program.cs
+++ Introducao_Ao_dotnet/Anotacoes/pasta7/Revisao/Program.cs
@@ -4,8 +4,77 @@ namespace Revisao
 {
     class Program
     {
-        private static void menu(){
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
 
+            menu();
+            string opcao = Console.ReadLine();
+
+            while(opcao.ToUpper() != "X")
+            {
+                switch(opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        // TODO: listar alunos
+                        foreach(var a in alunos)
+                        {
+                            if(!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for(int i = 0; i < alunos.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal += alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                menu();
+                opcao = Console.ReadLine();
+            }
+
+            Console.WriteLine("Saindo");
+        }
+
+         private static void menu(){
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada: ");
             Console.WriteLine("1- Inserir novo Aluno");
             Console.WriteLine("2- Listar ALunos");
@@ -13,36 +82,6 @@ namespace Revisao
             Console.WriteLine("X- Sair");
             Console.WriteLine();
 
-        }
-        static void Main(string[] args)
-        {
-            menu();
-
-            string opcaoUsuario = Console.ReadLine();
-
-            while(opcaoUsuario.ToUpper() != "X")
-            {
-                switch(opcaoUsuario)
-                {
-                    case "1":
-                        //TODO: adicionar aluno
-                        break;
-                    case "2":
-                        // TODO: listar alunos
-                        break;
-                    case "3":
-                        // TODO: calcular média geral
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                menu();
-
-                opcaoUsuario = Console.ReadLine();
-            }
-
-            Console.WriteLine("Saindo");
         }
     }
 }
